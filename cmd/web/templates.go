86717ae9cb2b18abeb36124f045a51bbd7298ce8
anukuljoshi <anukuljoshi1@gmail.com>
2023-09-30 20:57:43 +0530

    custom template function to format date

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -21,6 +21,17 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 	}
 }
 
+// convert time.Time to human readable format
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 03:04PM")
+}
+
+// initialize template.FuncMap object and store in global variable
+// lookup table for template function and our created functions
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	// initialize map to act as cache
 	cache := map[string]*template.Template{}
@@ -32,8 +43,10 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		// extract filename from path
 		name := filepath.Base(page)
+		// register template.FuncMap before calling ParseFiles() method
+		// create empty template set and register function with Funcs method
 		// parse base template into a template set
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
 		if err!=nil {
 			return nil, err
 		}
