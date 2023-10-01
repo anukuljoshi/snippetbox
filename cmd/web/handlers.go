9d9d9c3e01548c4eecaf60a130ee816b4f6ef045
anukuljoshi <anukuljoshi1@gmail.com>
2023-10-01 13:42:47 +0530

    added form to create snippets with form validations

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,10 +5,20 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.anukuljoshi/internals/models"
 )
+
+// struct to hold form data and field errors
+type snippetCreateForm struct {
+	Title string
+	Content string
+	Expires int
+	FieldErrors map[string]string
+}
 
 // handler for catch all
 func (app *application) home(w http.ResponseWriter,  r *http.Request){
@@ -49,16 +59,66 @@ func (app *application) viewSnippet(w http.ResponseWriter,  r *http.Request){
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display snippet form for creating new snippet"))
+	data := app.newTemplateData(r)
+	// initialize snippetCreateForm struct to pass to template
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
+	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
 // handler for creating a snippet
 func (app *application) createSnippetPost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n-Kobayashi Issa"
-	expires := 7
+	// call r.ParseForm() to parse and add POST request body data in r.PostForm map
+	err := r.ParseForm()
+	if err!=nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// use Get method on r.PostForm to get POST data
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	// r.PostForm.Get() return string
+	// convert string to int
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err!=nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// initialize instance of snippetCreateForm to hold form data and empty fields errors
+	form := snippetCreateForm{
+		Title: title,
+		Content: content,
+		Expires: expires,
+		FieldErrors: make(map[string]string),
+	}
+	// validations check for title
+	// 1. title is not empty
+	// 2. title is less than 100 characters
+	if strings.TrimSpace(form.Title)=="" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	}else if utf8.RuneCountInString(form.Title)>100 {
+		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+	// validations check for content
+	// 1. content is not empty
+	if strings.TrimSpace(form.Content)=="" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
+	// validation checks for expires
+	// expires should be either 1, 7 or 365
+	if form.Expires!=1 && form.Expires!=7 && form.Expires!=365 {
+		form.FieldErrors["form.Expires"] = "This field must be equal to 1, 7 or 365"
+	}
+	// return bad request if form.FieldErrors are present
+	if len(form.FieldErrors)>0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusBadRequest, "create.tmpl.html", data)
+		return
+	}
 	// call insert for snippet model with data
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err!=nil {
 		app.serverError(w, err)
 		return
